ec439b1fb98f4afa52473d6a11177d3a8305ff96
Levi Evans <levipierceevans@gmail.com>
2022-06-21 11:03:45 -0700

    change clientjs to socket

--- client.js
+++ client.js
@@ -1,17 +1,14 @@
 // Node.js socket client script
 const net = require('net');
 
-// Connect to a server @ port 9898
-const client = net.createConnection(5050, "fe80::4407:f169:6f4f:2e43%11", () => {
-  console.log('CLIENT: I connected to the server.');
-  client.write('CLIENT: Hello this is client!');
-});
+import { io } from "socket.io-client";
 
-client.on('data', (data) => {
-  console.log(data.toString());
-  client.end();
-});
 
-client.on('end', () => {
-  console.log('CLIENT: I disconnected from the server.');
+const socket = io("172.91.45.85", {
+
+});
+socket.on("connect", () =>{
+  socket.on("recieve_msg", () =>{})
+
+  socket.emit("send_msg", { a: "b", c: [] });
 });
