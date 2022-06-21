bbc07ab7024c5a93576e6591facfb00a38602880
Levi Evans <levipierceevans@gmail.com>
2022-06-21 13:07:25 -0700

    send msg to server send a constant message to a predefined server

--- client.js
+++ client.js
@@ -1,16 +1,22 @@
 const b = require("buffer");
 const io = require("socket.io-client");
 
+var net = require('net');
+
 const HEADER = 64;
 const PORT = 5050;
 const FORMAT = 'utf-8';
-const SERVER = "172.16.225.152";
+const SERVER = "172.16.226.95";
+
+var client = new net.Socket();
+
+
 
 socket = io.connect(`${SERVER}:${PORT}`);
 
 const byteSize = str => new b.Blob([str]).size;
 
-let msg = "nerdjuice";
+let msg = "nerdJUICESFEF";
 msg = Buffer.from(msg, 'utf-8').toString();
 console.log(msg);
 
@@ -29,8 +35,20 @@ while(true){
   myHeader += " ";
 }
 
-socket.send(myHeader);
-socket.send(msg);
+// socket.send(myHeader);
+// socket.send(msg);
 
+client.connect(PORT, SERVER, function() {
+  console.log('Connected');
+  console.log(`this is mh "${myHeader}"`);
+  client.write(myHeader);
+  console.log(`this is m ${msg}`);
+  client.write(msg);
+});
+
+client.on('data', function(data) {
+  console.log('Received: ' + data);
+  client.destroy(); // kill client after server's response
+});
 
 socket.disconnect()
