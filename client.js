bac85cad5a7ed6a5d3ef84bc4a132e4777a3cfc6
Levi Evans <levipierceevans@gmail.com>
2022-06-21 12:48:34 -0700

    connect to server connect to an address, calculate header, send header and message

--- client.js
+++ client.js
@@ -1,12 +1,36 @@
-const io =  require("socket.io-client");
+const b = require("buffer");
+const io = require("socket.io-client");
+
+const HEADER = 64;
+const PORT = 5050;
+const FORMAT = 'utf-8';
+const SERVER = "172.16.225.152";
+
+socket = io.connect(`${SERVER}:${PORT}`);
+
+const byteSize = str => new b.Blob([str]).size;
+
+let msg = "nerdjuice";
+msg = Buffer.from(msg, 'utf-8').toString();
+console.log(msg);
+
+let myHeader = byteSize(msg);
+
+console.log(myHeader);
+
+while(true){
+  if(byteSize(myHeader) == HEADER){
+    break;
+  }
+  if(myHeader > HEADER){
+    console.log("Error");
+    break;
+  }
+  myHeader += " ";
+}
+
+socket.send(myHeader);
+socket.send(msg);
 
 
-const socket = io("172.91.45.85", {
-
-});
-
-socket.on("connect", () =>{
-  socket.on("recieve_msg", () =>{})
-
-  socket.emit("send_msg", { a: "b", c: [] });
-});
+socket.disconnect()
