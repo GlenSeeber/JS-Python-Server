e5bf0a944ab650172f0fce88c995f19d45a10757
Levi Evans <levipierceevans@gmail.com>
2022-06-21 11:10:37 -0700

    clean code removed unused dependancy

--- client.js
+++ client.js
@@ -1,12 +1,10 @@
-// Node.js socket client script
-const net = require('net');
-
-import { io } from "socket.io-client";
+const io =  require("socket.io-client");
 
 
 const socket = io("172.91.45.85", {
 
 });
+
 socket.on("connect", () =>{
   socket.on("recieve_msg", () =>{})
 
